93728b4eabe459d5a378084754c0631d145be13b
Chrissa Philip <ge87gux@mytum.de>
2025-12-11 12:08:16 +0100

    Modified dataloader.py to also take custom collate function

--- component_classification_with_link_prediction_head/evaluation/FEGIN/dataloader.py
+++ component_classification_with_link_prediction_head/evaluation/FEGIN/dataloader.py
@@ -47,32 +47,37 @@ class DataLoader(torch.utils.data.DataLoader):
             reshuffled at every epoch. (default: :obj:`False`)
         follow_batch (list or tuple, optional): Creates assignment batch
             vectors for each key in the list. (default: :obj:`[]`)
+        collate_fn (callable, optional): Custom collate function. If None,
+            uses PyG default collate.
     """
     def __init__(self, dataset, batch_size=1, shuffle=False, follow_batch=[],
-                 **kwargs):
-        def collate(batch):
-            elem = batch[0]
-            if isinstance(elem, Data):
-                return Batch.from_data_list(batch, follow_batch)
-            elif isinstance(elem, float):
-                return torch.tensor(batch, dtype=torch.float)
-            elif isinstance(elem, int_classes):
-                return torch.tensor(batch)
-            elif isinstance(elem, string_classes):
-                return batch
-            elif isinstance(elem, container_abcs.Mapping):
-                return {key: collate([d[key] for d in batch]) for key in elem}
-            elif isinstance(elem, tuple) and hasattr(elem, '_fields'):
-                return type(elem)(*(collate(s) for s in zip(*batch)))
-            elif isinstance(elem, container_abcs.Sequence):
-                return [collate(s) for s in zip(*batch)]
+                 collate_fn=None, **kwargs):
+        if collate_fn is None:
+            def collate(batch):
+                elem = batch[0]
+                if isinstance(elem, Data):
+                    return Batch.from_data_list(batch, follow_batch)
+                elif isinstance(elem, float):
+                    return torch.tensor(batch, dtype=torch.float)
+                elif isinstance(elem, int_classes):
+                    return torch.tensor(batch)
+                elif isinstance(elem, string_classes):
+                    return batch
+                elif isinstance(elem, container_abcs.Mapping):
+                    return {key: collate([d[key] for d in batch]) for key in elem}
+                elif isinstance(elem, tuple) and hasattr(elem, '_fields'):
+                    return type(elem)(*(collate(s) for s in zip(*batch)))
+                elif isinstance(elem, container_abcs.Sequence):
+                    return [collate(s) for s in zip(*batch)]
 
-            raise TypeError('DataLoader found invalid type: {}'.format(
-                type(elem)))
+                raise TypeError('DataLoader found invalid type: {}'.format(
+                    type(elem)))
+            
+            collate_fn = collate  # use default
 
         super(DataLoader,
               self).__init__(dataset, batch_size, shuffle,
-                             collate_fn=lambda batch: collate(batch), **kwargs)
+                             collate_fn=collate_fn, **kwargs)
 
 
 class DataListLoader(torch.utils.data.DataLoader):
